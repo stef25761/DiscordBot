42306a82071b29e1b76c1ac96fabea8c1b7de147
stef25761 <wotan25761@googlemail.com>
2018-10-01 23:09:59 +0200

    Add poke to reg function

--- bot.py
+++ bot.py
@@ -1,20 +1,21 @@
 # Work with Python 3.6
-import discord
+import random
+
 from discord import Game
 from discord.ext.commands import Bot
-import random
+
 TOKEN = 'NDk2MzE3NTQwMTQyNjc4MDE2.DpPkVw.CIIy-G3g40c9sKJgIsCWX2oP7kE'
 BOT_PREFIX = ("?", "!")
 bot = Bot(command_prefix=BOT_PREFIX)
 
-client = discord.Client()
 
 @bot.command(descripton="Api key zur Serverwahl eingeben",
              brief="Api key zur Serverwahl eingeben",
              pass_context=True)
-async def reg(context):
-    msg  ='Einfach den API Key eingeben und dann wirst du zugeorndet! Bitte warten :P'
-    await bot.say(msg + ", " + context.message.author.mention)
+async def reg(ctx):
+    msg = "Einfach den API Key mir schicken und " \
+          "dann wirst du zugeorndet! Bitte warten :P"
+    await bot.send_message(ctx.message.author, msg)
 
 
 @bot.command(brief="Rate mal was passiert du Pleb!",
@@ -23,13 +24,9 @@ async def beleidigeMich(context):
     possible_brainfarts = ["Du Stinkst", "Geh dich Erhängen",
                            "Deine Mudda sammelt hässliche Kinder!",
                            "Ich kann schneller Radfahren als du!"]
-    await bot.say(random.choice(possible_brainfarts) + ", " + context.message.author.mention)
+    await bot.say(random.choice(
+        possible_brainfarts) + ", " + context.message.author.mention)
 
-@client.event
-async def on_server_join(server):
-
-    fmt = 'Welcome {0.mention} to {1.name}!'
-    await client.send_message(server, fmt.format(member, server))
 
 @bot.event
 async def on_ready():
@@ -38,5 +35,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
 
 bot.run(TOKEN)
